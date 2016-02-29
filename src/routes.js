74b0eaa82a4c38376c4c1bdd5b2b7376ae308bb6
Chris Savage <chrislsavage@gmail.com>
2016-02-28 21:43:06 -0500

    Add posts_index to routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
-import App from './components/app'
+import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 export default (
-<Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+  </Route>
 );
+
+ //lecture 75 indexroute means that when you go to the parent route you end up rendering the posts index as well
