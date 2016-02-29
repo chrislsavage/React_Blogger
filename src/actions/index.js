45b59c29f2688a15f9bbd0019886a2f36cc03b66
Chris Savage <chrislsavage@gmail.com>
2016-02-28 22:13:20 -0500

    set switch statement for reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-export const FETCH-POSTS = 'FETCH_POSTS';
+export const FETCH_POSTS = 'FETCH_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=savageasavageapost';
 
 export function fetchPosts() {
-  return {
-    const request = axios.get(`${ROOT_URL/posts${API_KEY}}`)
-  };
+
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
   return {
     type: FETCH_POSTS,
